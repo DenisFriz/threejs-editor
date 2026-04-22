6280cf59504bcf2fab9abb56813374c5d5905129
Denis <deni4gt@gmail.com>
2026-04-22 15:26:01 +0300

    Fixed a camera rotation when mesh is selected.

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -57,7 +57,7 @@ export default function Experience({
 
   return (
     <>
-      <OrbitControls makeDefault enabled={!selected} />
+      <OrbitControls makeDefault />
 
       <Lights />
       <Floor />
